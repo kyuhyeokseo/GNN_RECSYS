8c96e9cbf2073180743cbabb7ed8bf04ad8dbe8a
Kyuhyeok Seo <95399148+kyuhyeokGithub@users.noreply.github.com>
2022-07-04 16:40:46 +0900

    Modify Latent Factor Model

--- Netflix/SVD++.py
+++ Netflix/SVD++.py
@@ -7,17 +7,25 @@ import numpy as np
 from utils import path, pre_processing
 
 
-def predict_r_ui(mat, u, i, mu, bu, bi, qi, pu, N_u, yj):
-    N_u_sum = yj[N_u].sum(0)
-    return mu + bu[u] + bi[0, i] + np.dot(qi[i], (pu[u] + N_u_sum / sqrt(len(N_u))))
+def predict_r_ui(mat, u, i, mu, Bu, Bi, Q, P, N_u, Y):
+
+    p = P[u] + Y[N_u].sum(0) / np.sqrt(len(N_u))
+    Factor_part = np.dot(p, Q[i].T)
+
+    return mu + Bu[u] + Bi[i] + Factor_part
 
 
-def compute_error_ui(mat, u, i, mu, bu, bi, qi, pu, N_u, yj):
-    return mat[u, i] - predict_r_ui(mat, u, i, mu, bu, bi, qi, pu, N_u, yj)
+def compute_e_ui(mat, u, i, mu, Bu, Bi, Q, P, N_u, Y):
+
+    predict = predict_r_ui(mat, u, i, mu, Bu, Bi, Q, P, N_u, Y)
+
+    return mat[u, i] - predict
 
 
 def svd_more_more(mat, mat_file, f, gamma1=0.007, gamma2=0.007, l_reg6=0.005, l_reg7=0.015):
 
+    rmse = 0
+    cnt = 0
     # subsample the matrix to make computation faster
     mat = mat[0:mat.shape[0] // 128, 0:mat.shape[1] // 128]
     mat = mat[mat.getnnz(1) > 0][:, mat.getnnz(0) > 0]
@@ -28,59 +36,68 @@ def svd_more_more(mat, mat_file, f, gamma1=0.007, gamma2=0.007, l_reg6=0.005, l_
     bu_index, bi_index = pre_processing(mat, mat_file)
 
     # Init parameters
-    bu = np.random.rand(no_users, 1) * 2 - 1
-    bi = np.random.rand(1, no_movies) * 2 - 1
-    qi = np.random.rand(no_movies, f) * 2 - 1
-    pu = np.random.rand(no_users, f) * 2 - 1
-    yj = np.random.rand(no_movies, f) * 2 - 1
+    Bu = np.random.standard_normal(no_users)
+    Bi = np.random.standard_normal(no_movies)
+
+    Q = np.random.standard_normal((no_movies, f))
+    P = np.random.standard_normal((no_users, f))
+    Y = np.random.standard_normal((no_movies, f))
 
     mu = mat.data[:].mean()
 
     print("--- Optimizing ---")
-    n_iter = 100
+    n_iter = 30
 
     cx = mat.tocoo()
 
     for it in range(n_iter):
         for ind in range(len(cx.row)):
             u, i = cx.row[ind], cx.col[ind]
-            N_u = bi_index[u]
-            e_ui = compute_error_ui(mat, u, i, mu, bu, bi, qi, pu, N_u, yj)
 
-            bu[u] += gamma1 * (e_ui - l_reg6 * bu[u])
-            bi[0, i] += gamma1 * (e_ui - l_reg6 * bi[0, i])
-            qi[i] += gamma2 * (e_ui * (pu[u] + 1 / sqrt(len(N_u)) * yj[N_u].sum(0)) - l_reg7 * qi[i])
-            pu[u] += gamma2 * (e_ui * qi[i] - l_reg7 * pu[u])
-            yj[N_u] += gamma2 * (e_ui * 1 / sqrt(len(N_u)) * qi[i] - l_reg7 * yj[N_u])
+            N_u = bi_index[u]
+
+            e_ui = compute_e_ui(mat, u, i, mu, Bu, Bi, Q, P, N_u, Y)
+
+            Bu[u] = Bu[u] + gamma1 * (e_ui - l_reg6 * Bu[u])
+            Bi[i] = Bi[i] + gamma1 * (e_ui - l_reg6 * Bi[i])
+            Q[i] = Q[i] + gamma2 * ( e_ui * ( P[u] + (np.sum(Y[N_u], axis=0) )/np.sqrt(len(N_u)) ) - l_reg7 * Q[i] )
+            P[u] = P[i] + gamma2 * (e_ui * Q[i] - l_reg7 * P[u])
+            Y[N_u] = Y[N_u] + gamma2 * ( ((e_ui * Q[i]) / np.sqrt(len(N_u))).reshape(1, -1) - l_reg7 * Y[N_u] )
 
         gamma1 *= 0.9
         gamma2 *= 0.9
 
 
-    rmse = 0
-    cnt = 0
+        rmse = 0
+        cnt = 0
 
 
-    no_users = mat.shape[0]
-    no_movies = mat.shape[1]
-    for i in range(no_users):
-        for j in range(no_movies):
-            if (mat[i, j] != 0):
-                cnt += 1
-                N_u = bi_index[i]
-                rmse += ((mat[i, j] - predict_r_ui(mat, i, j, mu, bu, bi, qi, pu, N_u, yj)) ** 2)
-#                if (i+j)%10 == 0:
-#                    print(mat[i,j], predict_r_ui(mat, i, j, mu, bu, bi, qi, pu, N_u, yj), ((mat[i, j] - predict_r_ui(mat, i, j, mu, bu, bi, qi, pu,N_u, yj)) ** 2))
+        no_users = mat.shape[0]
+        no_movies = mat.shape[1]
+        for i in range(no_users):
+            for j in range(no_movies):
+                if (mat[i, j] != 0):
+                    cnt += 1
+                    N_u = bi_index[i]
+                    e_ui = compute_e_ui(mat, i, j, mu, Bu, Bi, Q, P, N_u, Y)
+
+                    rmse = rmse + (e_ui ** 2)
+    #                if (i+j)%10 == 0:
+    #                    print(mat[i,j], predict_r_ui(mat, i, j, mu, bu, bi, qi, pu, N_u, yj), ((mat[i, j] - predict_r_ui(mat, i, j, mu, bu, bi, qi, pu,N_u, yj)) ** 2))
+        print("iter : ", it, " / RMSE : ", sqrt(rmse / cnt))
+
 
     return (sqrt(rmse / cnt))
 
 
 
 def svd_mm():
-    mat_file = path + "/T.mat"
-    mat = io.loadmat(mat_file)['X']
+
     rmse_list = []
-    for i in [25,50,100] :
+    for i in (10,20,40,80):
+
+        mat_file = path + "/T.mat"
+        mat = io.loadmat(mat_file)['X']
         rmse_list.append(svd_more_more(mat, mat_file, i))
 
     print(rmse_list)
--- Netflix/SVD.py
+++ Netflix/SVD.py
@@ -9,13 +9,22 @@ import numpy as np
 from utils import path, pre_processing
 
 
-def predict_r_ui(mat, u, i, mu, bu, bi, qi, pu):
-    return mu + bu[u] + bi[0, i] + np.dot(qi[i], pu[u])
+def predict_r_ui(mat, u, i, mu, Bu, Bi, Q, P):
 
-def compute_error_ui(mat, u, i, mu, bu, bi, qi, pu):
-    return mat[u, i] - predict_r_ui(mat, u, i, mu, bu, bi, qi, pu)
+    return mu + Bu[u] + Bi[i] + np.dot(P[u], Q[i].T)
 
-def _svd(mat, mat_file, f, gamma=0.007, l_reg6 = 0.005, l_reg7 = 0.015):
+def compute_e_ui(mat, u, i, mu, Bu, Bi, Q, P):
+
+    predict = predict_r_ui(mat, u, i, mu, Bu, Bi, Q, P)
+
+    return mat[u, i] - predict
+
+
+def _svd(mat, mat_file, f, gamma, l_reg6 = 0.005, l_reg7 = 0.015):
+
+    rmse = 0
+    cnt = 0
+
     # subsample
     mat = mat[0:mat.shape[0] // 128, 0:mat.shape[1] // 128]
     mat = mat[mat.getnnz(1) > 0][:, mat.getnnz(0) > 0]
@@ -26,58 +35,48 @@ def _svd(mat, mat_file, f, gamma=0.007, l_reg6 = 0.005, l_reg7 = 0.015):
     bu_index, bi_index = pre_processing(mat, mat_file)
 
     # Init parameters
-    bu = np.random.rand(no_users, 1) * 2 - 1
-    bi = np.random.rand(1, no_movies) * 2 - 1
-    qi = np.random.rand(no_movies, f) * 2 - 1
-    pu = np.random.rand(no_users, f) * 2 - 1
+    Bu = np.random.standard_normal(no_users)
+    Bi = np.random.standard_normal(no_movies)
+    Q = np.random.standard_normal((no_movies, f))
+    P = np.random.standard_normal((no_users, f))
 
     mu = mat.data[:].mean()
 
-    n_iter = 100
+    n_iter = 30
     cx = mat.tocoo()
 
-    fq = [0 for z in range(no_users)]
 
     print("--- Optimizing ---")
     for it in range(n_iter):
         for u, i, v in zip(cx.row, cx.col, cx.data):
-            e_ui = compute_error_ui(mat, u, i, mu, bu, bi, qi, pu)
-            bu[u] += gamma * (e_ui - l_reg6 * bu[u])
-            bi[0, i] += gamma * (e_ui - l_reg6 * bi[0, i])
-            qi[i] += gamma * (e_ui * pu[u] - l_reg7 * qi[i])
-            pu[u] += gamma * (e_ui * qi[i] - l_reg7 * pu[u])
-            if v == 5 :
-                fq[u] += 1
 
+            e_ui = compute_e_ui(mat, u, i, mu, Bu, Bi, Q, P)
+
+            Bu[u] = Bu[u] + gamma * (e_ui - l_reg6 * Bu[u])
+            Bi[i] = Bi[i] + gamma * (e_ui - l_reg6 * Bi[i])
+            Q[i] = Q[i] + gamma * (e_ui * P[u] - l_reg7 * Q[i])
+            P[u] = P[i] + gamma * (e_ui * Q[i] - l_reg7 * P[u])
         gamma *= 0.9
-
-
-
-    rmse = 0
-    cnt = 0
-
-    no_users = mat.shape[0]
-    no_movies = mat.shape[1]
-    for i in range(no_users):
-        for j in range(no_movies):
-            if (mat[i, j] != 0):
-                cnt +=1
-                rmse += ((mat[i, j] - predict_r_ui(mat, i, j, mu, bu, bi, qi, pu)) ** 2)
-#                if (i+j)%10 == 0:
-#                    print(mat[i,j], predict_r_ui(mat, i, j, mu, bu, bi, qi, pu), ((mat[i, j] - predict_r_ui(mat, i, j, mu, bu, bi, qi, pu)) ** 2))
+        rmse = 0
+        cnt = 0
+        for u, i, v in zip(cx.row, cx.col, cx.data):
+            e_ui = compute_e_ui(mat, u, i, mu, Bu, Bi, Q, P)
+            rmse = rmse + (e_ui*e_ui)
+            cnt = cnt + 1
+        print("iter : ", it, " / RMSE : ", sqrt(rmse / cnt))
 
     return (sqrt(rmse / cnt))
 
 
 
+
+
+
 def svd_based():
+
     mat_file = path + "/T.mat"
     mat = io.loadmat(mat_file)['X']
-    rmse_list = []
-    for i in [25,50,100] :
-        rmse_list.append(_svd(mat, mat_file, i))
-
-    print(rmse_list)
+    print(_svd(mat, mat_file, 40, 0.007))
 
 
 
